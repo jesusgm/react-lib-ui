258b103c021004cb6f099e8735698cf6fc2176dc
Jesús Gándara Martínez <jesusalxen@gmail.com>
2021-02-23 23:59:05 +0100

    fix: 🐛 Update lib and docs to publish

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,26 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
-
-import "./styles.css";
-
-const propTypes = {
-  /**
-   * A extra classname
-   */
-  className: PropTypes.string,
-  /**
-   * Type: "primary" | "secondary" or "tertiary"
-   */
-  type: PropTypes.oneOf("primary", "secondary", "tertiary"),
-  /**
-   * Fn to execute on click
-   */
-  onClick: PropTypes.func,
-  /**
-   * The button content
-   */
-  children: PropTypes.any.isRequired,
-};
 
 const Button = (props) => {
   const { className, children, type, onClick } = props;
@@ -42,7 +20,5 @@ Button.defaultProps = {
   type: "primary",
   onClick: noop,
 };
-
-Button.propTypes = propTypes;
 
 export default Button;
